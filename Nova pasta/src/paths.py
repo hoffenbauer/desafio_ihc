fe36edac66ead5d9b4fbc82ab7bc74b0e0c68836
hoffenbauer <lucas.castrov@gmail.com>
2025-09-29 16:37:56 -0300

    Removes Nova pasta

--- Nova pasta/src/paths.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-parent_path = Path(__file__).resolve().parents[1]
-
-
-PARQUET_PATH = parent_path / "data" / "pontos_coleta_municipios_longo.parquet"
-DB_PATH = parent_path / "data" / "coletas.db"
